cac76a99914d44a5609da4693927760c051ad0d3
Jonathan Prado <jpradoar@gmail.com>
2024-05-12 19:01:11 +0000

    mod webpages

--- 06-webserver/www/index.php
+++ 06-webserver/www/index.php
@@ -1,132 +1,86 @@
 <?php
-$db_host = getenv('MARIADB_HOST');
-$db_name = getenv('MARIADB_DATABASE');
-$db_user = getenv('MARIADB_USER');
-$db_pass = getenv('MARIADB_PASSWORD');
-try{
-  $db_con = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
-  $db_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e){
-  echo $e->getMessage();
-}
-$stmt = $db_con->prepare("SELECT * FROM clients");
-$stmt->execute();
-?>
+  $db_host = getenv('MARIADB_HOST');
+  $db_name = getenv('MARIADB_DATABASE');
+  $db_user = getenv('MARIADB_USER');
+  $db_pass = getenv('MARIADB_PASSWORD');
+  try{
+    $db_con = new PDO("mysql:host={$db_host};dbname={$db_name}",$db_user,$db_pass);
+    $db_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch(PDOException $e){
+    echo $e->getMessage();
+  }
+  $stmt = $db_con->prepare("SELECT * FROM clients");
+  $stmt->execute();
+?>  
 <!DOCTYPE html>
 <html>
-<style>
-input[type=text], select {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-}
-input[type=submit] {
-  width: 100%;
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-input[type=submit]:hover {
-  background-color: #45a049;
-}
-div {
-  border-radius: 5px;
-  padding: 20px;
-}
-body {
-  background-color: #f2f2f2;
-}
-.table {
-    border-collapse: collapse;
-    margin: 25px 0;
-    font-size: 0.9em;
-    font-family: sans-serif;
-    min-width: 400px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-}
-.table th,
-.table td {
-    padding: 1% 15px;
-}
-.table tbody tr:last-of-type {
-    border-bottom: 2px solid #3383FF;
-}
-#messages {
-  border: 1px solid black;
-  padding: 10px;
-  margin-top: auto;
-  max-height: auto;
-  overflow: auto;
-}
-</style>
 <head>
-  <meta charset="UTF-8">
+<meta charset="UTF-8">
+<title>Database clients created by MQTT-Event</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+  body {
+      font-family: Arial, sans-serif;
+      background-color: #f2f2f2;
+  }
+  .container {
+      max-width: 95%;
+      margin: 20px auto;
+      background-color: #e0e5e8;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      padding: 20px;
+  }
+  table {
+      border-collapse: collapse;
+      margin: 25px 0;
+      font-family: Arial, sans-serif;
+      box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  }
+  th, td {
+      padding: 12px 15px;
+      text-align: left;
+      border-bottom: 1px solid #ddd;
+  }
+  th {
+      background-color: #4CAF50;
+      color: white;
+  }
+</style>
 </head>
-<body>    
-<div align="center">
-<img src="it-infrastructure-icon-28.jpg" height="60" width="60">
-<br><h3>Database clients created by MQTT-Event</h3>
-<table border="1" class="table">
-<tr>
-  <th scope="col"> ID </th>
-  <th scope="col"> Client</th>
-  <th scope="col"> ArchType</th>
-  <th scope="col"> Hardware</th>
-  <th scope="col"> Product</th>
-  <th scope="col"> Deployed</th>
-  <th scope="col"> License type</th>
-</tr>
-<?php while($row=$stmt->fetch(PDO::FETCH_ASSOC)) { ?>
-  <tr>
-  <td><?php echo $row['id']; ?></td>
-  <td><?php echo $row['client']; ?></td>
-  <td><?php echo $row['archtype']; ?></td>
-  <td><?php echo $row['hardware']; ?></td>    
-  <td><?php echo $row['product']; ?></td>  
-  <td><?php echo $row['xdate']; ?></td>
-  <td><?php echo $row['license']; ?></td>           
-  </tr>
-<?php   }   $stmt = null; ?>
-</table>                        
-
-<br><br><h3>DEPLOYMENT REAL TIME STATUS</h3>
-<ul id="messages"></ul>
-<script>
-  // Conexión a RabbitMQ
-  const QUEUE_NAME = 'event-status';
-  const AMQP_URL = 'ws://rabbitmq:15674/ws'; // Reemplazar con la URL de conexión a RabbitMQ
-
-  const client = Stomp.client(AMQP_URL);
-  client.connect('admin', 'admin', onConnect, onError);
-
-  function onConnect() {
-    console.log('Conectado a RabbitMQ');
-    client.subscribe(QUEUE_NAME, onMessageReceived);
-  }
-
-  function onError(error) {
-    console.error('Error de conexión a RabbitMQ:', error);
-  }
-
-  function onMessageReceived(message) {
-    const body = JSON.parse(message.body);
-    console.log('Mensaje recibido:', body);
-    const li = document.createElement('li');
-    li.innerText = JSON.stringify(body);
-    document.getElementById('messages').appendChild(li);
-  }
-</script>
-
+<body>
+<div class="container">
+    <div align="center">
+        <img src="it-infrastructure-icon-28.jpg" height="60" width="60">
+        <h3>Database clients created by MQTT-Event v1</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Client</th>
+                    <th>ArchType</th>
+                    <th>Hardware</th>
+                    <th>Product</th>
+                    <th>Deployed</th>
+                    <th>License type</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php while($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['client']; ?></td>
+                        <td><?php echo $row['archtype']; ?></td>
+                        <td><?php echo $row['hardware']; ?></td>
+                        <td><?php echo $row['product']; ?></td>
+                        <td><?php echo $row['xdate']; ?></td>
+                        <td><?php echo $row['license']; ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
 </div>
-
 </body>
 </html>
